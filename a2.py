d417b22b71134b4926595337756bb786559e877e
ARA34 <agreyesaranda@gmail.com>
2024-02-01 23:17:53 -0800

    commands moved to ui.py

--- a2.py
+++ a2.py
@@ -8,129 +8,16 @@
 # areyesar@uci.edu
 # STUDENT ID
 
+# TODO:
+# implement admin mode - still kinda iffy idk how they want us to integrate this...
+# 
+
 from pathlib import Path
 from ui import *
-        
-def recur_dir(directory: Path):
-    # input: directory
-    # output: list of paths
-    paths = []
-    for path in directory.iterdir():
-        if path.is_file():
-            paths.append(path)
-
-    dirs = []
-    for path in directory.iterdir():
-        if path.is_dir():
-            dirs.append(path)
-    for path in dirs:
-        paths.append(path)
-        paths.extend(recur_dir(path))
-    return paths 
-
-def paths_to_strs(path_list:list):
-    # input: list of paths
-    # output: string of path directories
-    output = ""
-    for path in path_list:
-        output += str(path) + "\n"
-
-    return output.strip()
 
 
-def command_L(directory: Path, subs: list, extra_input: str):
-    # input: diectory, list of sub commands, extra input
-    # output: string of paths
-    output = []
-    iter_paths = []
+def admin():
 
-    allowed_ext = [".dsu", ".py",".txt"]
-    for path in directory.iterdir():
-        iter_paths.append(path)
-
-    if len(subs) == 0 and extra_input == "":
-        output = paths_to_strs(iter_paths)
-    elif "-r" in subs:
-        # it r time
-        output = recur_dir(directory)
-        if "-f" in subs:
-            # r  + f
-            output = list(filter(lambda p: p.is_file(), output))
-            output = paths_to_strs(output)
-        elif "-e" in subs:
-            # r + e
-            if "."+extra_input in allowed_ext:
-                output = list(filter(lambda p: p.suffix == "." + extra_input, output))
-                output = paths_to_strs(output)
-            else:
-                output = "ERROR"
-        elif "-s" in subs:
-            # r + s
-            if extra_input != "":
-                output = list(filter(lambda p: p.name == extra_input, output))
-                output = paths_to_strs(output)
-            else:
-                output = "ERROR"
-        else:
-            output = paths_to_strs(output)
-    elif "-f" in subs:
-        # f
-        if extra_input == "":
-            output = list(filter(lambda p: p.is_file(), iter_paths))
-            output = paths_to_strs(output)
-        else:
-            output = "ERROR"
-    elif "-e" in subs:
-        # e
-        if "." + extra_input in allowed_ext:
-            output = list(filter(lambda p: p.suffix == "." + extra_input, iter_paths))
-            output = paths_to_strs(output)
-        else:
-            output = "ERROR"
-    elif "-s" in subs:
-        # s
-        if extra_input != "":
-            output = list(filter(lambda p: p.name == extra_input, iter_paths))
-            output = paths_to_strs(output)
-        else:
-            output = "ERROR"
-    else:
-        output = "ERROR"
-    return output
-
-
-def command_C(directory: Path, subs, filename):
-    output = ""
-    if "-n" in subs:
-        filename_dsu = filename + ".dsu"
-        file = directory/filename_dsu
-        file.touch()
-        output = str(file)
-    else:
-        output = "ERROR"
-    return output
-
-def command_D(file_dir: Path):
-    if file_dir.exists() and file_dir.suffix == ".dsu":
-        str_file = str(file_dir)
-        file_dir.unlink()
-        print(str_file + " DELETED")
-    else:
-        print("ERROR")
-
-def command_R(file_dir: Path):
-    contents = ""
-    if file_dir.suffix != ".dsu":
-        contents = "ERROR"
-    elif len(file_dir.read_text()) != 0:
-        contents = file_dir.read_text().strip()
-    else:
-        contents = "EMPTY"
-    return contents
-
-
-def main():
-    # did not have time to implement the multiple commands input
     usr_input = input()
     command_input, directory_input, subs, extra = parse_input(usr_input)
 
@@ -149,7 +36,21 @@ def main():
 
         usr_input = input()
         command_input, directory_input, subs, extra = parse_input(usr_input)
-        
+    
+
+def user():
+    pass
+
+def main():
+    # did not have time to implement the multiple commands input
+    print("Enter 'admin' or 'user'.\n")
+    usr_input = input().lower()
+
+    if usr_input == "admin":
+        admin() # run admin command
+    elif usr_input == "user":
+        user()
+        # run other command
         
 
 if __name__ == "__main__":
--- ui.py
+++ ui.py
@@ -9,6 +9,129 @@
 # STUDENT ID
 
 from pathlib import Path
+
+
+def recur_dir(directory: Path):
+    # input: directory
+    # output: list of paths
+    paths = []
+    for path in directory.iterdir():
+        if path.is_file():
+            paths.append(path)
+
+    dirs = []
+    for path in directory.iterdir():
+        if path.is_dir():
+            dirs.append(path)
+    for path in dirs:
+        paths.append(path)
+        paths.extend(recur_dir(path))
+    return paths 
+
+
+def paths_to_strs(path_list:list):
+    # input: list of paths
+    # output: string of path directories
+    output = ""
+    for path in path_list:
+        output += str(path) + "\n"
+
+    return output.strip()
+
+
+def command_L(directory: Path, subs: list, extra_input: str):
+    # input: diectory, list of sub commands, extra input
+    # output: string of paths
+    output = []
+    iter_paths = []
+
+    allowed_ext = [".dsu", ".py",".txt"]
+    for path in directory.iterdir():
+        iter_paths.append(path)
+
+    if len(subs) == 0 and extra_input == "":
+        output = paths_to_strs(iter_paths)
+    elif "-r" in subs:
+        # it r time
+        output = recur_dir(directory)
+        if "-f" in subs:
+            # r  + f
+            output = list(filter(lambda p: p.is_file(), output))
+            output = paths_to_strs(output)
+        elif "-e" in subs:
+            # r + e
+            if "."+extra_input in allowed_ext:
+                output = list(filter(lambda p: p.suffix == "." + extra_input, output))
+                output = paths_to_strs(output)
+            else:
+                output = "ERROR"
+        elif "-s" in subs:
+            # r + s
+            if extra_input != "":
+                output = list(filter(lambda p: p.name == extra_input, output))
+                output = paths_to_strs(output)
+            else:
+                output = "ERROR"
+        else:
+            output = paths_to_strs(output)
+    elif "-f" in subs:
+        # f
+        if extra_input == "":
+            output = list(filter(lambda p: p.is_file(), iter_paths))
+            output = paths_to_strs(output)
+        else:
+            output = "ERROR"
+    elif "-e" in subs:
+        # e
+        if "." + extra_input in allowed_ext:
+            output = list(filter(lambda p: p.suffix == "." + extra_input, iter_paths))
+            output = paths_to_strs(output)
+        else:
+            output = "ERROR"
+    elif "-s" in subs:
+        # s
+        if extra_input != "":
+            output = list(filter(lambda p: p.name == extra_input, iter_paths))
+            output = paths_to_strs(output)
+        else:
+            output = "ERROR"
+    else:
+        output = "ERROR"
+    return output
+
+
+def command_C(directory: Path, subs, filename):
+    output = ""
+    if "-n" in subs:
+        filename_dsu = filename + ".dsu"
+        file = directory/filename_dsu
+        file.touch()
+        output = str(file)
+    else:
+        output = "ERROR"
+    return output
+
+
+def command_D(file_dir: Path):
+    if file_dir.exists() and file_dir.suffix == ".dsu":
+        str_file = str(file_dir)
+        file_dir.unlink()
+        print(str_file + " DELETED")
+    else:
+        print("ERROR")
+
+
+def command_R(file_dir: Path):
+    contents = ""
+    if file_dir.suffix != ".dsu":
+        contents = "ERROR"
+    elif len(file_dir.read_text()) != 0:
+        contents = file_dir.read_text().strip()
+    else:
+        contents = "EMPTY"
+    return contents
+
+
 def parse_input(input:str):
     # L dir -r (3)
     # L dir (2)
@@ -62,4 +185,5 @@ def parse_input(input:str):
     return input_list
 
 
-        
+def recieve_input(parsed_input):
+    pass
