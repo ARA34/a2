82f44999047bb7842b16479f5a252c9d60526393
ARA34 <agreyesaranda@gmail.com>
2024-02-08 16:27:05 -0800

    majority of part 2 working now

--- a2.py
+++ a2.py
@@ -10,7 +10,9 @@
 
 # TODO:
 # implement admin mode - still kinda iffy idk how they want us to integrate this...
-# 
+# Stylecheck Profile.py
+# Finish editDSU function
+# refactor the admin function to include the new type of parsed inputs
 
 from pathlib import Path
 from ui import *
@@ -19,7 +21,7 @@ from ui import *
 def admin():
 
     usr_input = input()
-    command_input, directory_input, subs, extra = parse_input(usr_input)
+    command_input, directory_input, subs, extra = parse_input_general(usr_input)
     directory_input = Path(directory_input)
 
     while command_input != "Q":
@@ -29,31 +31,65 @@ def admin():
         if command_input == "L":
             print(command_L(directory_path, subs, extra))
         elif command_input == "C":
-            print(command_C(directory_path, subs, extra))
+            print(command_C_admin(directory_path, subs, extra))
         elif command_input == "D":
             command_D(directory_path)
         elif command_input == "R":
             print(command_R(directory_path))
 
         usr_input = input()
-        command_input, directory_input, subs, extra = parse_input(usr_input)
+        command_input, directory_input, subs, extra = parse_input_general(usr_input)
     
 
 def user():
+    # refactor code into list of tuples
     usr_input = input()
-    command_input, directory_input, subs, extra = parse_input(usr_input)
+    parsed_user_input = parse_inputs(usr_input)
+    command_input = parsed_user_input[0]
+    directory_input = parsed_user_input[1] 
+
     directory_input = Path(directory_input)
+    user_profile = Profile(username=None, password=None)
+    profile_loaded = False
+
+
     while command_input != "Q":
         if len(usr_input) == 1:
             print("ERROR")
         elif command_input == "C":
-            print(command_C(directory_input, subs, extra))
+            subs, extra = parsed_user_input[2:] # may be wrong
+            command_c = command_C(directory_input, subs, extra)
+            user_profile.username = command_c[1]
+            user_profile.password = command_c[2]
+            user_profile.bio = command_c[3]
+            profile_loaded = True # potential error(s)
+            print(command_input, directory_input, subs, extra)
+            # C /Users/alexra/Documents/UCI_WINTER_2023/ICS_32/test_folder -n myjournal1
         elif command_input == "O":
-            print(loadDSU()) # in progress
-            pass
+            try:
+                user_profile = loadDSU(directory_input)
+                profile_loaded = True
+            except:
+                print("Something went wrong. Profile not loaded.")
+                return
+
+        elif command_input == "E": # edit information once DSU file opened
+            # remove the directory form input only leave part with subs and sub inputs
+
+            # O /Users/alexra/Documents/UCI_WINTER_2023/ICS_32/test_folder/myjournal.dsu
+            # E /Users/alexra/Documents/UCI_WINTER_2023/ICS_32/test_folder/myjournal.dsu -usr Reese -pwd thisisnewpassword
+            # print(command_input, directory_input, subs, extra)
+            tup_list = parsed_user_input[2]
+            if profile_loaded:
+                editDSU(tup_list, user_profile)
+            else:
+                print("There is no profile loaded, please run C or O command.")
+                return
 
         usr_input = input()
-        command_input, directory_input, subs, extra = parse_input(usr_input)
+        parsed_user_input = parse_inputs(usr_input)
+        command_input = parsed_user_input[0]
+        directory_input = parsed_user_input[1] 
         # C /Users/alexra/Documents/UCI_WINTER_2023/ICS_32/test_folder -n myjournal
 
 def main():
--- ui.py
+++ ui.py
@@ -2,8 +2,6 @@
 
 # Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
 
-# Replace the following placeholders with your information.
-
 # Alex Reyes Aranda
 # areyesar@uci.edu
 # STUDENT ID
@@ -39,7 +37,7 @@ def paths_to_strs(path_list:list):
 
     return output.strip()
 
-
+# NR need refactor
 def command_L(directory: Path, subs: list, extra_input: str):
     # input: diectory, list of sub commands, extra input
     # output: string of paths
@@ -101,6 +99,18 @@ def command_L(directory: Path, subs: list, extra_input: str):
     return output
 
 
+def command_C_admin(directory: Path, subs, filename):
+    output = ""
+    if "-n" in subs:
+        filename_dsu = filename + ".dsu"
+        file = directory/filename_dsu
+        file.touch()
+        output = str(file)
+    else:
+        output = "ERROR"
+    return output
+
+# takes directory, subs, and filename NR need refactor
 def command_C(directory: Path, subs, filename):
     output = ""
     if "-n" in subs:
@@ -118,9 +128,9 @@ def command_C(directory: Path, subs, filename):
         user.save_profile(file) # takes the file directory as input
     else:
         output = "ERROR"
-    return output
+    return output, username, userpass, userbio
 
-
+# takes directory NNR - No need refactor
 def command_D(file_dir: Path):
     if file_dir.exists() and file_dir.suffix == ".dsu":
         str_file = str(file_dir)
@@ -129,7 +139,7 @@ def command_D(file_dir: Path):
     else:
         print("ERROR")
 
-
+#takes directory NNR
 def command_R(file_dir: Path):
     contents = ""
     if file_dir.suffix != ".dsu":
@@ -141,12 +151,15 @@ def command_R(file_dir: Path):
     return contents
 
 
-def parse_input(input:str):
+# INPUT PARSING FUNCTIONS
+
+def parse_input_general(input:str):
     # L dir -r (3)
     # L dir (2)
     # C dir -n something (4) dir dir2 something 
     # L dir -r -e dsu (5)
     # L dir -r -f
+
     input_list = []
 
     command_letter = ""
@@ -193,9 +206,128 @@ def parse_input(input:str):
 
     return input_list
 
-def loadDSU(dsu_path: Path):
-    
-    pass
 
-def recieve_input(parsed_input):
-    pass
+def parse_input_edit(o_input:str):
+    # input: String o_input
+    # output: List tuples matching sub with sub input
+
+    # O dir -usr Reese -pwd 4321
+    # C dir -n myjournal
+    
+    # Walkthrough:
+    # input: -usr Reese R -pwd 4321
+    # usr, Reese R, pwd, 4321
+    # usrReeseRpwd4321
+    # usr, Reese, R pwd 4321
+    # _, Reese R, _ 4321
+    # _ 
+    parsed_list = []
+
+
+    command_letter = o_input[0]
+    o_input = o_input[2:]
+    dir_input = o_input.split("-")[0]
+    dir_input = "".join(dir_input).strip()
+
+    allowed_subs = ["-usr", "-pwd", "-bio", "-addpost", "-delpost", "-r","-f","-s","-e","-n"]
+    allowed_subs_stripped = list(map(lambda d: d[1:], allowed_subs)) # commands without "-"
+
+    subs = list(filter(lambda i: i in allowed_subs, o_input.split())) # getting all the commands
+
+    input_split = o_input.split("-")[1:]
+    input_split = "".join(input_split).split()
+    for i in range(len(input_split)):
+        if input_split[i] in allowed_subs_stripped:
+            input_split[i] = "_"
+
+    input_split = " ".join(input_split)
+    input_split = input_split.split("_")[1:]
+    input_split = list(map(lambda d:d.strip(), input_split))
+
+    tpl_list = list(map(lambda x,y: (x,y), subs, input_split))
+    parsed_list = [command_letter, dir_input, tpl_list]
+    return parsed_list
+
+
+def parse_inputs(user_input:str):
+    allowed_general = ["-r","-f","-s","-e","-n"]
+    allowed_o = ["-usr", "-pwd", "-bio", "-addpost", "-delpost"]
+
+    in_general = list(map(lambda d: d in user_input, allowed_general))
+    in_allowed_o = list(map(lambda d: d in user_input, allowed_o))
+    parsed_list = [] # what to be returned
+    
+    if True in in_general:
+        # converst to old format --> can be refactored but not enough time
+        parsing_list = parse_input_edit(user_input)
+        tup_list = parsing_list[2]
+
+        subs = list(map(lambda d: d[0], tup_list)) # working
+        extra_input = list(map(lambda s: s[1], tup_list))
+        extra_input = list(filter(lambda s: s != "", extra_input))[0]
+        parsed_list = [parsing_list[0], parsing_list[1], subs, extra_input]
+
+    elif True in in_allowed_o:
+        parsed_list = parse_input_edit(user_input)
+    else:
+        parsed_list = parse_input_edit(user_input)[:-1]
+        pass # do whatever else
+    return parsed_list
+
+
+def loadDSU(dsu_path: Path):
+    profile_to_load = Profile(dsu_path)
+    profile_to_load.load_profile(dsu_path)
+    print("DSU file loaded.")
+    print(profile_to_load.username, profile_to_load.password, profile_to_load.bio)
+    return profile_to_load
+    
+
+def save_note(note: str, p: Path):
+    # check if storage file exists, if not create it.
+    
+    # open and write user note to file
+    f = p.open('a')
+    f.write(note + '\n')
+    f.close()
+
+
+def edit_by_command(tup:tuple, userprofile: Profile):
+    # load the saved information onto a list
+    # assume the file is loaded
+    # input: One tuple at a time
+    sub = tup[0]
+    new = tup[1]
+    if sub == "-usr":
+        # edit the username part of the loaded list
+        userprofile.username = new
+        print("username edited to: ", new)
+    elif sub == "-pwd":
+        # edit the password part of the loaded list
+        userprofile.password = new
+        print("password edited to: ", new)
+    elif sub == "bio":
+        # edit the bio part of the loaded list
+        userprofile.bio = new
+        print("bio edited to: ", new)
+    elif sub == "-addpost":
+        # add a post to dsu file
+        # userprofile.add_post()
+        pass
+    elif sub == "-delpost": 
+        # deete a file from dsu file based on index
+        pass
+
+
+def editDSU(tup_list: list, userprofile: Profile):
+    # can editDSU only if the file is loaded by C or O command -> i.e the file exists
+    # either one command at a time or multiple
+    for tup in tup_list:
+        # run the edit command
+        edit_by_command(tup, userprofile)
+        print("Username: ", userprofile.username, " Password: ", userprofile.password," Bio: ", userprofile.bio, " Posts: ", userprofile._posts)
+    
+
+    # input: Path DSU
+    # output: Str message of updated things
+
